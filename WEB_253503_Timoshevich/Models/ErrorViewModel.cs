844b9251daeafa04f6f004eacbdc0bb3b4f4c39e
Karina Timoshevich <karina03tima@gmail.com>
2024-09-15 13:04:20 +0300

    LabRab 1

---
+++ WEB_253503_Timoshevich/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_253503_Timoshevich.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
